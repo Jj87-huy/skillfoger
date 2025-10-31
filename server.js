cb7e00fdbc7e6d744eb481e8ea1a4fb125490192
Jj87-huy <haoallain964@gmail.com>
2025-11-01 05:59:34 +0700

    Fix GitHub URL for fetching HTML

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/get/html", async (req, res) => {
   }
 
   try {
-    const githubUrl = "https://raw.githubusercontent.com/Jj87-huy/skillfoger/refs/heads/main/index.html";
+    const githubUrl = "https://raw.githubusercontent.com/Jj87-huy/skillfoger/main/index.html";
     const response = await fetch(githubUrl);
     let html = await response.text();
 
